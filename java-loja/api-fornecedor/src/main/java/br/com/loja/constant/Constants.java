48b3f3131baf4352018477d91a2a00e62f0e4800
Danilo <danilo@waycomp.com.br>
2020-06-06 16:16:50 -0300

    ajuste loja

--- java-loja/api-fornecedor/src/main/java/br/com/loja/constant/Constants.java
+++
@@ -1,6 +0,0 @@
-package br.com.loja.constant;
-
-public class Constants {
-    public static final String API_URL = "loja/api/v1/";
-    public static final String API_CLIENTE = API_URL + "cliente";
-}
--- java-loja/api-fornecedor/src/main/java/br/com/loja/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.loja.repository;
-
-import br.com.loja.model.Cliente;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClienteRepository extends MongoRepository<Cliente, Integer> {
-}
